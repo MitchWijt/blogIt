3440c45e9a00f1309e370aafc1f139a4c82e41c8
MitchWijt <mitchel@wijt.net>
2022-04-28 11:20:37 +0200

    feat: add comment service and upvote functionality

--- src/main/java/com/example/blogit/comment/CommentController.java
+++ src/main/java/com/example/blogit/comment/CommentController.java
@@ -2,20 +2,34 @@ package com.example.blogit.comment;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/blog/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+
     @PostMapping("/create")
-    public Comment createComment(@RequestBody Comment comment) {
-        // let the comment service create a new comment in the DB
-        return comment;
+    public Comment createComment(@Valid @RequestBody Comment comment) {
+        return commentService.createComment(comment);
+    }
+
+    @PutMapping("/upvote/{uuid}")
+    public Comment upvoteComment(@PathVariable("uuid") UUID commentUUID) throws Exception {
+        return commentService.upvoteComment(commentUUID);
     }
 
     @GetMapping("/{blogUUID}")
-    public void getComments(@PathVariable("blogUUID") UUID blogUUID) {
-        // make the service get all comments that have a blog UUID.
-        // return list of comments
+    public List<Comment> getComments(@PathVariable("blogUUID") UUID blogUUID) {
+        System.out.println(blogUUID);
+        return commentService.getComments(blogUUID);
     }
 }
